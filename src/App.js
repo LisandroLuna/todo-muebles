0881df15c6dce8fa5b600d25f1361e5778d24c23
Lisandro <lisandro_luna@ymail.com>
2020-10-21 19:49:35 -0300

    Barra de navegación.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar></NavBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo Muebles - Sitio en Construcción.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/LisandroLuna"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          By Lisandro Luna - CoderHouse ReactJs
         </a>
       </header>
     </div>
---
+++ src/components/navBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <ul className="menu">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    );
+}
+
+export default NavBar;

e885de8959692372398a85a0dc95e0551d7f24c4
Lisandro <lisandro_luna@ymail.com>
2020-11-02 20:34:19 -0300

    Se actualiza como funcion y se corrigen errores.

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,59 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/ItemCount.css';
 
-class ItemCount extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            itemNumber: this.props.initial,
-        };
-    }
+function ItemCount(props){
+    // Declaro contador
+    const [itemNumber, setItemNumber] = useState(props.initial);
 
-    // Cuento Items
-    count = (a) =>{
-        a ? this.setState({ itemNumber: this.state.itemNumber + 1}) : this.setState({ itemNumber: this.state.itemNumber - 1});
-    }
-
-    // Bloqueo botones de acuerdo Items Maximos y Minimos
-    lockBtn = () =>{
-        // Minimo 1 y Maximo 10
-        console.log(this.state.itemNumber)
-        if(this.state.itemNumber >= 10){
-            document.getElementById("plusBtn").disabled = true;
-        }else{
-            document.getElementById("plusBtn").disabled = false;
-        }
-        if(this.state.itemNumber <= 1){
-            document.getElementById("lessBtn").disabled = true;
-        }else{
-            document.getElementById("lessBtn").disabled = false;
-        }
-    }
-
-    // Ejecuto al montar item
-    componentDidMount(){
-        this.lockBtn();
-    }
-
-    // Ejecuto al hacer cambios
-    componentDidUpdate(){
-        this.lockBtn();
-    }
-
-    render(){
-        return(
+    return(
+        <div className="col text-center">
+            <p>
+                <button id="plusBtn" className="btn btn-success ml-auto" onClick={() => setItemNumber(itemNumber + 1)} disabled={itemNumber === props.max}>+</button>
+                <b value={itemNumber} className="m-4">{itemNumber}</b>
+                <button id="lessBtn" className="btn btn-danger mr-auto" onClick={() => setItemNumber(itemNumber - 1)} disabled={itemNumber === props.min}>-</button>
+            </p>
             <div className="col text-center">
-                <p>
-                    <button id="plusBtn" className="btn btn-success ml-auto" onClick={() => this.count(true)}>+</button>
-                    <b value={this.state.itemNumber} className={"m-4"}>{this.state.itemNumber}</b>
-                    <button id="lessBtn" className="btn btn-danger mr-auto" onClick={() => this.count(false)}>-</button>
-                </p>
-                <div className="col text-center">
-                    <button onClick={() => this.props.onAdd()} className="btn btn-info ml-auto mr-auto">Agregar al carrito</button>
-                </div>
+                <button onClick={() => props.onAdd()} className="btn btn-info ml-auto mr-auto">Agregar al carrito</button>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ItemCount;
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,23 +2,21 @@ import React from 'react';
 import '../assets/Product.css';
 import ItemCount from "./ItemCount";
 
-class Product extends React.Component{
+function Product(){
 
-    addItem = () => {
+    function addItem(){
         console.log("Items added to cart!");
     }
 
-    render(){
-        return (
-            <div className="card ml-auto mr-auto mt-5 mb-5">
-                <div className="card-body">
-                    <h5 className="card-title">Ropero</h5>
-                    <p className="card-text">Ropero de pino con 3 puertas.</p>
-                    <ItemCount initial={1} min={0} max={15} onAdd={this.addItem}/>
-                </div>
+    return (
+        <div className="card ml-auto mr-auto mt-5 mb-5">
+            <div className="card-body">
+                <h5 className="card-title">Ropero</h5>
+                <p className="card-text">Ropero de pino con 3 puertas.</p>
+                <ItemCount initial={1} min={1} max={10} onAdd={addItem}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Product;

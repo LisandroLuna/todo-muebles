819824712da650839dab0f832425e4f76922f5bf
Lisandro <lisandro_luna@ymail.com>
2020-11-04 10:42:15 -0300

    Se agrega spinner.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,25 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import '../assets/ItemList.css';
 import Item from "./Item";
 
 function ItemList(props){
     const { items } = props;
-    const [itemList, setItemList] = useState([]);
-
-    const getItemProm = (data) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {return resolve(data)}, 2000);
-        })
-    };
-
-    useEffect( () =>{
-            getItemProm(items).then(result => {setItemList(result)});
-            console.log('Data');
-        }, [itemList, items]);
 
     return (
         <div className="row row-cols-1 row-cols-md-3">
-            {itemList.map(item =>
+            {items.map(item =>
                 <Item key={item.id} prod={item}/>
             )}
         </div>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,30 @@
 import '../assets/Home.css';
-import React  from "react";
+import React, {useEffect, useState} from "react";
 import ItemList from "../components/ItemList";
-import itemList from "../data/muebles.json"
+import itemData from "../data/muebles.json"
+import Spinner from "../components/Spinner";
 
 function Home({greeting}) {
+    const [itemList, setItemList] = useState([]);
+    const [loadState, setLoadState] = useState(true);
+
+    const getItemProm = (data) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {return resolve(data)}, 2000);
+        })
+    };
+
+    useEffect( () =>{
+        getItemProm(itemData).then(result => {setItemList(result)}).finally(() => {setLoadState(false)});
+        console.log('Data');
+    }, []);
+
     return (
         <div className={"col-lg-12 home"}>
             <h1 className={"display-4 text-center"}>Todo Muebles</h1>
             <p className={"lead text-center"}>{ greeting }</p>
-            <div className={"col-md-9 mx-auto"}>
-                <ItemList items={itemList}/>
+            <div className={"col-md-9 mx-auto p-5"}>
+                {loadState ? <Spinner/> : <ItemList items={itemList}/>}
             </div>
         </div>
     );

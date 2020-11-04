ebc2efc6fd7046ad6630f4ba24addb8eaa1fc8db
Lisandro <lisandro_luna@ymail.com>
2020-11-04 11:18:07 -0300

    Updates

---
+++ precache-manifest.c5a76d79011f8cdc6e9f46d2943ae3e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f00ea06147242df17edbdf8cf73a86f",
+    "url": "/todo-muebles/index.html"
+  },
+  {
+    "revision": "2937dc48606779744b75",
+    "url": "/todo-muebles/static/css/main.39c28325.chunk.css"
+  },
+  {
+    "revision": "b8deec83835f0148e944",
+    "url": "/todo-muebles/static/js/2.6a709414.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-muebles/static/js/2.6a709414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2937dc48606779744b75",
+    "url": "/todo-muebles/static/js/main.a1ad09a2.chunk.js"
+  },
+  {
+    "revision": "9e5eb57a860e0c8086a7",
+    "url": "/todo-muebles/static/js/runtime-main.75cbf59e.js"
+  }
+]);

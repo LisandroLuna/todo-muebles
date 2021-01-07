6979ae475199a18ddea36cf0f0fcfc8166f3006f
Lisandro <lisandro_luna@ymail.com>
2021-01-06 21:11:26 -0300

    Se ocultan credenciales de FB

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyCmlsXLxY4KyE5tarZ05sTxfwOJxgALSsU",
-    authDomain: "todo-muebles.firebaseapp.com",
-    databaseURL: "https://todo-muebles.firebaseio.com",
-    projectId: "todo-muebles",
-    storageBucket: "todo-muebles.appspot.com",
-    messagingSenderId: "79353251862",
-    appId: "1:79353251862:web:d07faee0974383ba060d37",
-    measurementId: "G-V5E31J89JM"
+//Config de firebase
 });
 
 export function getFirebase(){
